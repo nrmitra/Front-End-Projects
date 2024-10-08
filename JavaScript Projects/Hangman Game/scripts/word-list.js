b2cd117c806b154c20fbcf64621938a4c734a54e
nrmitra <neel.ratan.mitra.1991@gmail.com>
2024-10-08 15:44:38 +0530

    added hangman game code

---
+++ JavaScript Projects/Hangman Game/scripts/word-list.js
@@ -0,0 +1,23 @@
+const wordList = [
+    {
+        word: "science",
+        hint: "A field of study related to innovation & discovery"
+    },
+    {
+        word: "coffee",
+        hint: "Popular drink usually drank for energy and alertness"
+    },
+    {
+        word: "science",
+        hint: "A field of study related to innovation & discovery"
+    },
+    {
+        word: "dance",
+        hint: "A way to express enjoyment. Rhythmic body movement."
+    },
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings and played with a pick."
+    }
+    
+];
