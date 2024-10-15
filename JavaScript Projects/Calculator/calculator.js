e2cd7fcc407cb7bdd2dc4fa65a9f915bd7102352
nrmitra <neel.ratan.mitra.1991@gmail.com>
2024-10-15 15:32:11 +0530

    added code for calculations for js exercise

--- JavaScript Projects/Calculator/calculator.js
+++ JavaScript Projects/Calculator/calculator.js
@@ -4,6 +4,7 @@ let ouputVal = 0;
 let output;
 
 const outputScreen = document.querySelector(".output-screen");
+outputScreen.innerHTML = Number(0);
 
 function calculate(numberValue){
     
@@ -14,15 +15,39 @@ numberBtn.forEach(
     btn => btn.addEventListener("click",function(e) {
         let buttonValue = e.target.value;
 
-        if (buttonValue == '='){
+        if (buttonValue == 'C'){
+            outputScreen.innerHTML = Number(0);
+
+        }
+
+        else if (buttonValue == '='){
             outputScreen.innerHTML = calculate(numberVal);
             numberVal = Number(0);
         }
         else{
+            numberVal = outputScreen.textContent;
 
+            if((buttonValue == '+') || numberVal.includes('+') || 
+            (buttonValue == '-') || numberVal.includes('-') ||
+            (buttonValue == '*') || numberVal.includes('*') ||
+            (buttonValue == '/') || numberVal.includes('/') ||  
+            (buttonValue == '.') || numberVal.includes('.')
+            ){
+                
+                numberVal = numberVal + buttonValue;
+                outputScreen.innerHTML = numberVal;
+                
+                
+            }
+            else{
+                
+                numberVal = Number(numberVal + buttonValue);
+                outputScreen.innerHTML = numberVal;
+
+            }
             
-            numberVal = numberVal + buttonValue;
-            outputScreen.innerHTML = numberVal;
+            
+            
 
         }
 
